6ad764af9276427615ce794b17e5b462d65b7f38
LunFengChen <108616973+LunFengChen@users.noreply.github.com>
2026-06-27 13:46:46 -0500

    fix: allow ROM auto-install builds to use local signing

--- app/src/main/java/me/bmax/apatch/APatchApp.kt
+++ app/src/main/java/me/bmax/apatch/APatchApp.kt
@@ -340,7 +340,8 @@ class APApplication : Application(), Thread.UncaughtExceptionHandler {
             exitProcess(0)
         }
 
-        if (!BuildConfig.DEBUG && !verifyAppSignature("1x2twMoHvfWUODv7KkRRNKBzOfEqJwRKGzJpgaz18xk=")) {
+        if (!BuildConfig.DEBUG && !BuildConfig.AUTO_INSTALL_APATCH &&
+            !verifyAppSignature("1x2twMoHvfWUODv7KkRRNKBzOfEqJwRKGzJpgaz18xk=")) {
             while (true) {
                 val intent = Intent(Intent.ACTION_DELETE)
                 intent.data = "package:$packageName".toUri()
